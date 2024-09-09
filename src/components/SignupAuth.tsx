3e7632e283d1c1fe1845f54486e1a4d7d1a68bc0
SwatejReddy <swatejreddy17@gmail.com>
2024-09-09 18:21:19 +0530

    removed margin on top of the title for signup for mobile screens

--- src/components/SignupAuth.tsx
+++ src/components/SignupAuth.tsx
@@ -4,7 +4,7 @@ export const SignupAuth = () => {
     return (
         <div className="flex justify-center items-center min-h-screen px-4 sm:px-6 lg:px-8">
             <div className="w-full max-w-sm sm:w-3/4 md:w-1/2 lg:w-1/3 xl:w-1/4">
-                <div className="text-center mt-5 mb-2">
+                <div className="text-center mt-5 mb-2 sm:mt-0">
                     <div className="font-bold mb-1 text-2xl sm:text-3xl">
                         Create an account
                     </div>
