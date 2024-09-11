13193a591539aa1fd2239daef59467dcb854bb41
SwatejReddy <swatejreddy17@gmail.com>
2024-09-11 12:49:00 +0530

    Add navbar to the create rapt page

--- src/pages/CreateRapt.tsx
+++ src/pages/CreateRapt.tsx
@@ -13,6 +13,7 @@ import {
     AlertDialogHeader,
     AlertDialogTitle,
 } from "@/components/ui/alert-dialog"
+import { Navbar } from "@/components/Navbar"
 
 export const CreateRapt = () => {
     const [title, setTitle] = useState('')
@@ -50,46 +51,50 @@ export const CreateRapt = () => {
     }
 
     return (
-        <div className="flex items-center w-full flex-col p-4 max-h-full">
-            <div className="w-3/4 h-16 text-3xl font-bold mb-4">
-                <input onChange={(e) => { setTitle(e.target.value) }}
-                    className="h-full w-full outline-none placeholder-gray-500"
-                    type="text"
-                    placeholder="Enter your title here"
-                />
+        <>
+            <Navbar />
+            <div className="flex items-center w-full flex-col p-4 max-h-full">
+
+                <div className="w-3/4 h-16 text-3xl font-bold mb-4">
+                    <input onChange={(e) => { setTitle(e.target.value) }}
+                        className="h-full w-full outline-none placeholder-gray-500"
+                        type="text"
+                        placeholder="Enter your title here"
+                    />
+                </div>
+                <div className="w-3/4 mb-5">
+                    <textarea onChange={(e) => { setContent(e.target.value) }} className="w-full min-h-96 text-base leading-relaxed outline-none placeholder-gray-500" placeholder="Start writing your content here..."></textarea>
+                </div>
+                <div className="content-start w-3/4">
+                    {
+                        loading ?
+                            (
+                                <Button disabled>
+                                    <ReloadIcon className="mr-2 h-4 w-4 animate-spin" />
+                                    Posting
+                                </Button>
+                            ) :
+                            (
+                                <Button onClick={submitRapt} variant="default">Post</Button>
+                            )
+                    }
+                </div>
+                <AlertDialog open={showAlert}>
+                    <AlertDialogContent className="flex flex-col items-center justify-center max-w-[300px] p-4">
+                        <AlertDialogHeader className="text-center">
+                            <AlertDialogTitle className="text-lg font-semibold mb-2">Invalid Input</AlertDialogTitle>
+                            <AlertDialogDescription className="text-sm text-muted-foreground">
+                                Title or Content cannot be empty.
+                            </AlertDialogDescription>
+                        </AlertDialogHeader>
+                        <AlertDialogFooter className="flex justify-center mt-4">
+                            <AlertDialogAction onClick={() => setShowAlert(false)}>
+                                Okay
+                            </AlertDialogAction>
+                        </AlertDialogFooter>
+                    </AlertDialogContent>
+                </AlertDialog>
             </div>
-            <div className="w-3/4 mb-5">
-                <textarea onChange={(e) => { setContent(e.target.value) }} className="w-full min-h-96 text-base leading-relaxed outline-none placeholder-gray-500" placeholder="Start writing your content here..."></textarea>
-            </div>
-            <div className="content-start w-3/4">
-                {
-                    loading ?
-                        (
-                            <Button disabled>
-                                <ReloadIcon className="mr-2 h-4 w-4 animate-spin" />
-                                Posting
-                            </Button>
-                        ) :
-                        (
-                            <Button onClick={submitRapt} variant="default">Post</Button>
-                        )
-                }
-            </div>
-            <AlertDialog open={showAlert}>
-                <AlertDialogContent className="flex flex-col items-center justify-center max-w-[300px] p-4">
-                    <AlertDialogHeader className="text-center">
-                        <AlertDialogTitle className="text-lg font-semibold mb-2">Invalid Input</AlertDialogTitle>
-                        <AlertDialogDescription className="text-sm text-muted-foreground">
-                            Title or Content cannot be empty.
-                        </AlertDialogDescription>
-                    </AlertDialogHeader>
-                    <AlertDialogFooter className="flex justify-center mt-4">
-                        <AlertDialogAction onClick={() => setShowAlert(false)}>
-                            Okay
-                        </AlertDialogAction>
-                    </AlertDialogFooter>
-                </AlertDialogContent>
-            </AlertDialog>
-        </div>
+        </>
     )
 }
